fc7aa7019f01030e0be2c18ea271b989b6489dcf
Ahmid <ahmidsanni@gmail.com>
2025-03-27 16:21:15 +0100

    add logo and made some changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
+import { NavLink } from "react-router";
+
 function Header() {
   return (
     <header className="h-fit z-50 py-2 border-b border-black/4 flex items-center justify-between fixed inset-0 w-full bg-[#F0FDFF] 2xl:container 2xl:mx-auto px-5 lg:px-7 xl:px-10">
-      <h1 className="uppercase cursor-pointer font-sora text-xl xl:text-2xl italic text-[#105418] font-bold tracking-[1px]">
+      {/* <h1 className="uppercase cursor-pointer font-sora text-xl xl:text-2xl italic text-[#105418] font-bold tracking-[1px]">
         hdi.
-      </h1>
-
-      <nav>
-        <ul></ul>
-      </nav>
+      </h1> */}
+      <div>
+        <NavLink to="/">
+          <img src="/src/assets/images/HDI-Logo.png" className="w-14 md:w-12 xl:w-14 h-8.5 md:h-7 xl:h-10" />
+        </NavLink>
+      </div>
 
       <button className="font-cabin cursor-pointer py-2 xl:py-3 text-base md:text-[15px] lg:text-base xl:text-lg  px-5 lg:px-6 xl:px-8 rounded-2xl bg-[#105418] text-center">
         Get In Touch
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,23 +1,24 @@
 function Footer() {
   return (
     <footer className="bg-[#f0fff4] w-full pb-3 p-5 lg:p-7 xl:p-10">
-      <section className="rounded-[40px] bg-[#105418] px-2 md:px-5 lg:px-7 xl:px-10 pt-9 md:pt-14 lg:pt-19 xl:pt-25 pb-2 lg:pb-2.5 xl:pb-4">
+      <section className="rounded-[40px] bg-[#105418] px-2 md:px-5 lg:px-7 xl:px-10 pt-8 md:pt-14 lg:pt-19 xl:pt-25 pb-2 lg:pb-2.5 xl:pb-4">
         <div className="md:w-[66.7vw] mx-auto text-center space-y-6 md:space-y-5 lg:space-y-6 xl:space-y-8">
           <div>
             <h1 className="text-[22px] md:text-[34px] lg:text-[42px] xl:text-[56px] font-semibold md:-tracking-[1.5px] lg:-tracking-[2.6px] leading-8 md:leading-11 lg:leading-14 xl:leading-[74px]">
-              Let’s Build A Sustainable Future <br /> Together
+              Let’s Build A Sustainable Future{" "}
+              <br className="hidden md:block" /> Together
             </h1>
 
-            <p className="pt-2 md:pt-2.5 xl:pt-4 font-cabin text-base lg:text-lg xl:text-[20px] leading-6.5 md:leading-5.5 lg:leading-6 xl:leading-7.5">
+            <p className="pt-2 md:pt-2.5 xl:pt-4 font-cabin text-[15px] md:text-base lg:text-lg xl:text-[20px] leading-6.5 md:leading-5.5 lg:leading-6 xl:leading-7.5">
               Partner with HDI to drive impact in food, energy, agribusiness,
               and education.
             </p>
           </div>
 
-          <button className="font-cabin  text-lg lg:text-xl xl:text-2xl font-semibold leading-6 bg-black py-3.5 xl:py-4 px-6 md:px-5 lg:px-6 xl:px-8 rounded-2xl cursor-pointer">
+          <button className="font-cabin text-base md:text-lg lg:text-xl xl:text-2xl font-semibold leading-6 bg-black py-3.5 xl:py-4 px-6 md:px-5 lg:px-6 xl:px-8 rounded-2xl cursor-pointer">
             Get in Touch
           </button>
-          <p className="text-base md:text-sm xl:text-base">
+          <p className="text-[15px] md:text-sm xl:text-base">
             HDI Group © 2025 • All Rights Reserved
           </p>
         </div>
