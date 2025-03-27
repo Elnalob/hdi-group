1a2b37f5869edd9ca173c1016d3aec98c66a6f20
Ahmid <ahmidsanni@gmail.com>
2025-03-27 21:38:58 +0100

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import Footer from "./components/footer";
 import HeroSection from "./components/home/heroSection";
 import About from "./components/home/about";
 import Goals from "./components/home/goals"
+import Header from "./components/header";
 // import Header from './components/header'
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <HeroSection />
       <About />
       <Goals />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,10 @@ function Header() {
     <header className="h-fit z-50 py-2 border-b border-black/4 flex items-center justify-between fixed inset-0 w-full bg-[#F0FDFF] 2xl:container 2xl:mx-auto px-5 lg:px-7 xl:px-10">
       <div>
         <NavLink to="/">
-          <img src="/src/assets/images/HDI-Logo.png" className="w-14 md:w-12 xl:w-14 h-8.5 md:h-7 xl:h-10" />
+          <img
+            src="/src/assets/images/HDI-Logo.png"
+            className="w-14 md:w-12 xl:w-14 h-8.5 md:h-7 xl:h-10"
+          />
         </NavLink>
       </div>
 
