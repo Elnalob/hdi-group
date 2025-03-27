750bff8bc6516991a1bea4607312f8824ebd9666
Ahmid <ahmidsanni@gmail.com>
2025-03-27 21:30:04 +0100

    made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Header from "./components/header";
+// import Header from "./components/header";
 import Footer from "./components/footer";
 import HeroSection from "./components/home/heroSection";
 import About from "./components/home/about";
 import Goals from "./components/home/goals"
+import Header from '../src/components/header'
 
 function App() {
   return (
