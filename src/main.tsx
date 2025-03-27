49cd9167ec7d35c9cc9f3d774a91ec2e213fc980
Ahmid <ahmidsanni@gmail.com>
2025-03-27 09:29:45 +0100

    install router package and implement it

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-// import App from "./App.tsx";
+import App from "./App.tsx";
 import Contact from "./pages/Contact.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Contact />
+    <RouterProvider router={router} />
   </StrictMode>
 );
